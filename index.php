0c3ee329f1c0b6fad6372a8049432fff56d41eac
Danielfinx <daniel.e.san.1999@gmail.com>
2023-07-11 23:40:02 -0400

    Rework a toda la estructura del proyecto

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
-<html>
-    <head>
-        <title>CASINO</title>
-    </head>
-    <body>
-        <h3 align= "center">Casino Home</h3>
-        <?php
-            include ("menu.html");
-        ?>
-        <br><br><br>
-        <div = "imagen" align = "center">
-            <img src= casino.jpg>
-        </div>
-    </body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>PokerHUB</title>
+  </head>
+  <body>
+    <!--==========================================================-->
+    <!--ENCABEZADO-->
+    <!--==========================================================-->
+
+    <?php
+        include ("includes/header.html");
+    ?>
+
+    <!--==========================================================-->
+    <!--SLIDER-->
+    <!--==========================================================-->
+
+    <?php
+        include ("includes/slider.html");
+    ?>
+    
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
 </html>
