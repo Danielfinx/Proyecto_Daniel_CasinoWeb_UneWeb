54416a0fbeb92fb883ddb2e3e1dcbc5777929a99
Danielfinx <daniel.e.san.1999@gmail.com>
2023-07-12 11:00:20 -0400

    Ajustes de formato y añadido de secciones

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         <span class="text-primary fs-5 fw-bold"> PokerHUB </span>
-    </a>
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
---
+++ includes/servicios.php
@@ -0,0 +1,14 @@
+<section class="container-fluid">
+    <div class="row w-75 mx-auto servicio-fila">
+        <div class="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-start my-5">
+            <img src="img/imagen 2.png" alt="imagen" width="180" height="160">
+            <h3 class="fs-5 mt-4 px-4 pb-1">UNA IMAGEN</h3>
+            <p class="px-4">es una imagen</p>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-start my-5">
+            <img src="img/imagen 2.png" alt="imagen" width="180" height="160">
+            <h3 class="fs-5 mt-4 px-4 pb-1">UNA IMAGEN</h3>
+            <p class="px-4">es una imagen</p>
+        </div>
+    </div>
+</section>
