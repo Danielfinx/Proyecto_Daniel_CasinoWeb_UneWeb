187418a04bff08ed0a32ad79697eb8d799532d15
Danielfinx <daniel.e.san.1999@gmail.com>
2023-07-14 21:50:49 -0400

    Pequeños cambios

--- includes/intro.php
+++ includes/intro.php
@@ -1,4 +1,4 @@
 <section class="w-50 mx-auto text-center pt-5" id="intro">
-    <h1 class="p-3 fs-2 border-top border-3">Un Casino unico para satisfacer toda tu Ludopatía<span class="text-primary"> Casino Online</span></h1>
+    <h1 class="p-3 fs-2 border-top border-3">Un Casino unico para satisfacer todas tus ganas de apostar<span class="text-primary"> Casino Online</span></h1>
     <p class="pt-3 fs-4"><span class="text-primary">PokerHUB</span> Gasta toda tu plata AQUÍ</p>
 </section>
